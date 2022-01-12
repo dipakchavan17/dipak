44bef5c37fd4d14538eb4ccc80081455da09fa98
Dipak@1432 <chavandipakr17@gmail.com>
2022-01-12 11:03:01 +0530

    practice programs

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome");
+            Console.WriteLine("helloworld");
+            Console.ReadLine();
+            FibonacciSeries fibonacci = new FibonacciSeries();
+            fibonacci.FindFibonacciSeries();
+            Console.ReadLine();
+        }
+    }
+}
